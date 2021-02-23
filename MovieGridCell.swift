1e3fce9af1b2e1a524d2babd8416526e7636dc2b
cleoaustein <62226800+cleoaustein@users.noreply.github.com>
2021-02-23 12:51:24 -0500

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Cleo Austein on 2/22/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+ 
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
